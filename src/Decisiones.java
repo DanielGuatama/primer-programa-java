c29d57b05ecb584adbd89f443bd9c13fd90003a1
Daniel Guatama <edaniel.guatama@gmail.com>
2024-09-13 15:51:19 -0500

    Condiciones

---
+++ src/Decisiones.java
@@ -0,0 +1,21 @@
+public class Decisiones {
+
+    public static void main(String[] args) {
+        int fechaDeLanzamiento = 1999;
+        boolean incluidoEnELPlan = true;
+        double notaDeLaPelícula = 8.2;
+        String tipoPlan = "plus";
+
+        if (fechaDeLanzamiento >= 2022){
+            System.out.println("Peliculas más populares");
+        } else {
+            System.out.println("Peliculas retro que aún vale la pena ver");
+        }
+
+        if (incluidoEnELPlan && tipoPlan.equals("plus")){
+            System.out.println("Disfrute de su pelicula");
+        } else {
+            System.out.println("Pelicula no disponible para su plan actual");
+        }
+    }
+}
